686683f87eff3001c8f79457901f67d316b9b402
rishad004 <rishadmuthu004@gmail.com>
2024-05-30 11:18:15 +0530

    testing

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
 func main() {
-	
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hi guys fiber done")
+	})
+
+	log.Fatal(app.Listen(":2999"))
 }
